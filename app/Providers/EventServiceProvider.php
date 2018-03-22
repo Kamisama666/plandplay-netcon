acc50466bf3cc1ee8188fcdc7ff4b3cb6406bba7
Kamisama666 <andaynotemenes@gmail.com>
2018-03-22 20:37:38 +0000

    Added Email Unregistered

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -19,6 +19,9 @@ class EventServiceProvider extends ServiceProvider
         'App\Events\PlayerRegistered' => [
             'App\Listeners\SendPlayerRegisteredEmail',
         ],
+        'App\Events\PlayerUnregistered' => [
+            'App\Listeners\SendPlayerUnregisteredEmail',
+        ],
         'App\Events\WaitlistPlayerRegistered' => [
             'App\Listeners\SendEmailWaitlistPlayerRegistered',
         ],
