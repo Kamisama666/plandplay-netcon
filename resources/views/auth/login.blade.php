31126ca2b56638a532dd8d3ee61fc4bb03807392
Kamisama666 <kamisama666@openmailbox.org>
2019-03-25 22:31:01 +0000

    Changed login icon and text

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,17 +3,17 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-md-offset-4">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-defaultl" id="panel-login">
                 <div class="panel-heading">
                     <h3>BIENVENIDX A NETCONPlay</h3>
                      <p>
-                        <img src="{{ asset('img/logotipo-netcon-transparente_AZUL.png') }}">
+                        <img src="{{ asset('img/2019-03-25.png') }}">
                     </p>
                 </div>
                 <div class="panel-body">
                      <p>
-                        Bienvenid@ a NETCONPlay, la <strong>plataforma de gestión de partidas de las <a href="http://netconplay.com" title="Visitar netconplay.com" target="_blank">NETCON19</a>.</strong> Para registrar tus partidas o acceder a las que ya has subido solo tienes que loguearte con tu dirección de correo y contraseña.
+                        Bienvenid@ a NETCONPlay, la <strong>plataforma de gestión de partidas de las <a href="http://netconplay.com" title="Visitar netconplay.com" target="_blank">NETCON19</a>.</strong> Debes <a href="{{ route('register') }}" title="Registrarte en las Netcon" >registrarte</a> para subir tus partidas o acceder a las que ya has subido. Si ya estas registrado solo tienes que loguearte con tu dirección de correo y contraseña.
                     </p>
                     <form class="form-horizontal" method="POST" action="{{ route('login') }}">
                         {{ csrf_field() }}
@@ -70,7 +70,7 @@
 
                         <p>
                                 <a href="{{ route('register') }}" class="btn btn-danger">
-                                    Crear Nuevo Usuario
+                                    Registrar Nuevo Usuario
                                 </a>
 
                         </p>
